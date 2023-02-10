a79e86550faef19a40f750112222868f188bc2a7
Vamsi-rit <90811739+Vamsi-rit@users.noreply.github.com>
2023-02-10 14:54:24 +0530

    Create POSITIVE NEGATIVE.java

---
+++ DAY-1/POSITIVE NEGATIVE.java
@@ -0,0 +1,24 @@
+public class negative
+{
+
+public static void main(String[] args)
+{
+// TODO Auto-generated method stub
+int num=-15;
+if(num>0)
+{
+System.out.println("The number given is positive");
+}
+else if(num<0)
+{
+System.out.println("The number given is negative");
+}
+else
+{
+System.out.println("The Number given is zero");
+}
+
+
+}
+
+}
