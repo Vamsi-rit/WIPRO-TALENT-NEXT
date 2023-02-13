fc4c99cdfb26315f3e199b3aefff794a6a2957e0
Vamsi-rit <90811739+Vamsi-rit@users.noreply.github.com>
2023-02-13 09:49:41 +0530

    Update PROGRAMS3.java

--- DAY-6/PROGRAMS3.java
+++ DAY-6/PROGRAMS3.java
@@ -22,14 +22,20 @@ SUM OF POWER OF DIGITS
 import java.io.*;
 import  java.util.*;
 
-class SumOfSumsOfDigitsInCyclicOrder {
-    public int sumOfSumsOfDigits(int input1){
+class SumOfPowersOfDigits {
+    public int sumOfPowerOfDigits(int input1){
+        if (input1 <= 9) return 0;
+
         String num = String.valueOf(input1);
         int sum = 0;
 
         for (int i = 0; i < num.length(); i++) {
-            for (int j = i; j < num.length(); j++) {
-                sum += Integer.parseInt(String.valueOf(num.charAt(j)));
+            if (i == num.length() - 1) {
+                sum += 1;
+                System.out.println(num.charAt(i) + " ^ " + 0);
+            } else {
+                sum += Math.pow(Integer.parseInt(String.valueOf(num.charAt(i))),
+                        Integer.parseInt(String.valueOf(num.charAt(i + 1))));
             }
         }
         return sum;
